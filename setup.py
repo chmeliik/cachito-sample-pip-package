3ef3b3e073567f98b26ea2c59afd8d05284f3b38
Adam Cmiel <acmiel@redhat.com>
2020-11-04 15:49:41 +0100

    Put a slash in the package name

--- setup.py
+++ setup.py
@@ -1,2 +1,7 @@
 from setuptools import setup, find_packages
-setup(name='cachito-sample-pip-package', version='1.0.0', install_requires=['requests'], packages=find_packages())
+setup(
+    name='namespace/cachito-sample-pip-package',
+    version='1.0.0',
+    install_requires=['requests'],
+    packages=find_packages(),
+)
